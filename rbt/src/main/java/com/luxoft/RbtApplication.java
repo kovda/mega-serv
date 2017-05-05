2c3a7255cd660b33480d9a9eb71bde0cfa109726
Dmitry A. Kovalenko <DAKovalenko@luxoft.com>
2017-05-05 16:46:51 +0300

    initial

---
+++ rbt/src/main/java/com/luxoft/RbtApplication.java
@@ -0,0 +1,11 @@
+package com.luxoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbtApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RbtApplication.class, args);
+    }
+}
